bc46b19800ce6ad8a8b2f9f8fdd0f5c75d3b0912
Ana Cecília <136384443+ceciibat@users.noreply.github.com>
2023-08-01 17:41:44 -0400

    Update ContractService.cs
    explicações dadas a partir do comentário do Pedro na Udemy

--- Services/ContractService.cs
+++ Services/ContractService.cs
@@ -4,11 +4,15 @@ namespace Fix.Services
 {
      class ContractService 
     {
-        private IOnlinePaymentService _onlinePaymentService;                // instanciei uma variável do tipo da interface.
-
+        private IOnlinePaymentService _onlinePaymentService;                // isso serve para esta classe possuir uma instância da Interface
+        //               ^^
+        // o valor desta || vai ser dado justamente pelo construtor! 
+        // ela é instanciada na classe como atributo pois assim ela pode ser utilizada por vários Métodos
+        // ela é private por questões de segurança.
+          
         public ContractService(IOnlinePaymentService onlinePaymentService)
         {
-            _onlinePaymentService = onlinePaymentService;                   // que sera paypalservice
+            _onlinePaymentService = onlinePaymentService;                   // que sera paypalservice = UPCASTING (CASTING IMPLÍCITO)
         }
 
         public void ProcessContract(Contract contract, int months)          // processamento do contrato
@@ -18,14 +22,10 @@ namespace Fix.Services
             
             for (int i = 1; i <= months; i++)
             {
-                // aqui ele adiciona 1 mês a data inicial
-                DateTime date = contract.DateInitial.AddMonths(i);          
-                // variavel para atualizar a cota, somando nela o valor dos juros (interest)
-                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
-                //  variavel c/ valor final, é somada ao updateQuota a taxa de pagamento (paymentfee)
-                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);
-                // aqui pega o contract e adiciona uma parcela na lista
-                contract.AddInstallment(new Installment(date, fullQuota));
+                DateTime date = contract.DateInitial.AddMonths(i);                                     // aqui ele adiciona 1 mês a data inicial                    
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);      // variavel para atualizar a cota, somando nela o valor dos juros (interest)             
+                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);      //  variavel c/ valor final, é somada ao updateQuota a taxa de pagamento (paymentfee) 
+                contract.AddInstallment(new Installment(date, fullQuota));                             // aqui pega o contract e adiciona uma parcela na lista
 
             }
         }
